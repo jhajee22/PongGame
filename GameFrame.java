6f65851e34fef38a0534442bba980a534fa61b33
Saurav Kumar Jha <122507083+jhajee22@users.noreply.github.com>
2023-05-31 01:49:39 +0530

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+    GameFrame()
+     {
+        setTitle("Pong Game");
+        //setLayout(null);
+        getContentPane().setBackground(Color.black);
+        GamePanel panel = new GamePanel();
+        add(panel); // USED TO ADD PANEL INSIDE FRAME
+       // setSize(1000,555);
+       // setFocusable(true);
+         pack();
+        setResizable(false);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //THIS LINE COZ EXECUTION OF PROGRAM WILL STOP AFTER HITTING CROSS BUTTON
+
+    }
+    public static void main(String[] args) {
+        GameFrame g = new GameFrame();
+
+    }
+
+
+}
